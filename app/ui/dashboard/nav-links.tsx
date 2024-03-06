0641bf3bb4ec9be4c6949a606b95bf2bbdc92151
Chris Maki <c.william.developer@gmail.com>
2024-03-06 16:37:13 -0600

    Make sure nav-links goes to dashboard/customer or dashboard/customers, whichever the folder and file is called

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -13,7 +13,7 @@ const links = [
     href: '/dashboard/invoices',
     icon: DocumentDuplicateIcon,
   },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: 'Customers', href: '/dashboard/customer', icon: UserGroupIcon },
 ];
 
 export default function NavLinks() {
