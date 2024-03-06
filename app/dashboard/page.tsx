40a7d0e4c86e16a5ff5d40f09fb93d32b2d08a8f
Chris Maki <c.william.developer@gmail.com>
2024-03-06 17:17:37 -0600

    Replace nav-links a tag with Link tag, imported from next/link

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,10 @@
+import Link from 'next/link';
+
 export default function Page() {
-    return <p>Dashboard Page</p>;
+    return (
+<>
+        <p>Dashboard Page</p>
+        <Link href="/login"> Log in </Link> 
+        </>
+    )
 }
