55b4c2859b8c401fed8c3804abb44005d2ce9da4
Chris Maki <c.william.developer@gmail.com>
2024-03-06 18:17:43 -0600

    Config manually to account for Invoices case sensitivity

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    async rewrites() {
+      return [
+        {
+          source: '/dashboard/invoices',
+          destination: '/dashboard/Invoices',
+        },
+      ];
+    },
+  };
 module.exports = nextConfig;
