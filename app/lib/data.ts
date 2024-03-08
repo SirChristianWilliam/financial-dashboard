47aa3ff2f83e6c3c2e603ef114fd161e945a11f0
Chris Maki <c.william.developer@gmail.com>
2024-03-08 13:54:04 -0600

    In latest-invoices, change the export function to a simple await call, and import fetchLatestInvvoices from lib/data

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -39,6 +39,9 @@ export async function fetchLatestInvoices() {
   noStore();
 
   try {
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
