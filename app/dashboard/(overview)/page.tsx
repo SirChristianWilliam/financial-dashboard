2b8ef28f81951f160e98e26730a08f73ac27d8fe
Chris Maki <c.william.developer@gmail.com>
2024-03-09 11:44:51 -0600

    Wrap CardWrapper and Suspense in a div element for layout classes

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -21,9 +21,12 @@ export default async function Page() {
       <h1 className={`${myFonts.lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+
       <Suspense fallback={<CardsSkeleton />}>
           <CardWrapper />
         </Suspense>
+        </div>
         
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton />}>
