3ce95804a65ed40ff033127eb8ee5df5c96f9377
Chris Maki <c.william.developer@gmail.com>
2024-03-12 15:01:17 -0500

    Install next/auth as well as create app/login/page.tsx for routing to the login form page

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -6,6 +6,7 @@ import {
 } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
+// import { useFormState } from 'react-dom';
 
 export default function LoginForm() {
   return (
