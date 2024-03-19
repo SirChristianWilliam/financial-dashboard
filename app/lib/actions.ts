0df325c4611e7b905556286875a5349069d3da4e
Chris Maki <c.william.developer@gmail.com>
2024-03-19 14:32:42 -0500

    Import signIn and AuthError in actions.ts. Add async funciton authenticate to handle invalid log in credentials. Switch login-form.tsx to a client component. Import useFormState and authenticat and useFormStatus. Define an errorMessage and dispatch using useFormState, and sync the form action to equal the value of the dispatch

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,7 +3,9 @@ import { z } from "zod";
 import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
-// import { signIn } from "@/auth";
+import { signIn } from "@/auth";
+import { AuthError } from 'next-auth';
+
 
 export type State = {
   errors?: {
@@ -14,26 +16,24 @@ export type State = {
   message?: string | null;
 };
 
-// export async function authenticate(
-//   prevState: string | undefined,
-//   formData: FormData
-// ) {
-//   let responseRedirectUrl = null;
-//   try {
-//     responseRedirectUrl = await signIn("credentials", {
-//       ...Object.fromEntries(formData),
-//       redirect: false
-//     });
-//   } catch (error) {
-//     console.log("error", error);
-//     if ((error as Error).message.includes("CredentialsSignin")) {
-//       return "CredentialSignin";
-//     }
-//     throw error;
-//   } finally {
-//     if (responseRedirectUrl) redirect(responseRedirectUrl);
-//   }
-// }
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
 
 const InvoiceSchema = z.object({
   id: z.string(),
