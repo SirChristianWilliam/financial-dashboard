1b0a09e266072ecc7e2a607e81e32001ad221d04
Chris Maki <c.william.developer@gmail.com>
2024-03-27 13:36:42 -0500

    fetchFilteredCustomers so that the customers table will include the invoices data per each customer

--- app/dashboard/customer/page.tsx
+++ app/dashboard/customer/page.tsx
@@ -5,11 +5,12 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { myFonts } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
-import { fetchInvoicesPages, fetchCustomers } from '@/app/lib/data';
+import { fetchInvoicesPages, fetchCustomers, fetchFilteredCustomers } from '@/app/lib/data';
 import { Metadata } from 'next';
 import {
   CustomersTableType,
   FormattedCustomersTable,
+  CustomerField,
 } from '@/app/lib/definitions';
 
 
@@ -19,38 +20,38 @@ export const metadata: Metadata = {
 
 export default async function Page() {
   const customers = await fetchCustomers(); // Fetch the customers data
+  const customersFiltered = await fetchFilteredCustomers("");
 
   return (
     <>
-    <div className="w-full">
-      <div className="flex w-full items-center justify-between">
-        <h1 className={`${myFonts.lusitana.className} text-2xl`}>Customers</h1>
-      </div>
+      <div className="w-full">
+        <div className="flex w-full items-center justify-between">
+          <h1 className={`${myFonts.lusitana.className} text-2xl`}>Customers</h1>
+        </div>
 
-      <div className="mt-5 flex w-full justify-center">
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Email</th>
-              {/* Add more table headers if needed */}
-            </tr>
-          </thead>
-          <tbody>
-            {customers.map((customer) => (
-              <tr key={customer.id}>
-                <td>{customer.name}</td>
-                <td>{customer.email}</td>
-                {/* Add more table cells for other customer properties */}
+        <div className="mt-5 flex w-full justify-center">
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                {/* Add more table headers if needed */}
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {customers.map((customer) => (
+                <tr key={customer.id}>
+                  <td>{customer.name}</td>
+                  <td>{customer.email}</td>
+                  {/* Add more table cells for other customer properties */}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+
       </div>
-      
-    </div>
-          <Table customers={customers as FormattedCustomersTable[]} />
-          <p>yo</p>
-              </>
+      <Table customers={customersFiltered as FormattedCustomersTable[]} />    </>
   );
 }
+
