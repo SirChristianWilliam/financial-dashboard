2f01fc79866ec30387975438cc32e14b6add0e39
Chris Maki <c.william.developer@gmail.com>
2024-03-27 14:48:26 -0500

    Begin adding signup form page/links

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,8 +1,7 @@
 import AcmeLogo from '@/app/ui/acme-logo';
-import LoginForm from '@/app/ui/login-form';
-import Link from 'next/link';
-
-export default function LoginPage() {
+import SignUpForm from '../ui/signup-form';
+ 
+export default function SignupPage() {
   return (
     <main className="flex items-center justify-center md:h-screen">
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
@@ -11,10 +10,8 @@ export default function LoginPage() {
             <AcmeLogo />
           </div>
         </div>
-        <LoginForm />
+        <SignUpForm />
       </div>
-
-      <Link href={'/signup'} >Sign Up</Link>
     </main>
   );
 }
