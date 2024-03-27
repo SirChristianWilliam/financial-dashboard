5db028ad42718c67fa0f5e70a125c5b002568f84
Chris Maki <c.william.developer@gmail.com>
2024-03-27 15:09:29 -0500

    Add ArrowRightIcon

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -94,7 +94,7 @@ function LoginButton() {
       <Link
         href={'/signup'}
         className="flex h-10 items-center rounded-lg bg-blue-500 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:bg-blue-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50 mt-4 w-full">
-        Sign Up
+        Sign Up <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
       </Link>
     </>
   );
